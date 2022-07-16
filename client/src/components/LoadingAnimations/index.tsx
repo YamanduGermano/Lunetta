849e11fc53ce2824746f01bc730861aa7475c07e
Erick Passos <erickcassianopassos@gmail.com>
2022-07-16 19:32:57 -0300

    Animações componentizadas

---
+++ client/src/components/LoadingAnimations/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { View } from "react-native";
+import LottieView from 'lottie-react-native';
+
+export const BigLoading: React.FC = () => {
+	return (
+		<View
+			style={{
+				width: '100%',
+				height: '100%',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
+			<LottieView
+				style={{ width: '80%' }}
+				source={require('../../assets/Animations/Loading.json')}
+				autoPlay={true}
+				loop={true}
+			/>
+		</View>
+	);
+};
